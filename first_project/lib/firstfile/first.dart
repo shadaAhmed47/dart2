72740d2667de1f177cdece53762233239f497f8f
shadaAhmed47 <115115874+shadaAhmed47@users.noreply.github.com>
2024-03-09 03:30:25 +0200

    Add files via upload

---
+++ first_project/lib/firstfile/first.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+import 'dart:io';
+void main(List<String>arguments) {
+  // task 1
+  // int? n = int.parse(stdin.readLineSync()!);
+  // if(n%5==0){
+  //   print('Fizz');
+  // }
+  // else if(n%3==0)
+  //   {
+  //     print('Buzz');
+  //   }
+  // else
+  //   {
+  //     print('FIZZBUZZ');
+  //   }
+
+  // task 2
+  // int? x = int.parse(stdin.readLineSync()!);
+  // int? y = int.parse(stdin.readLineSync()!);
+  // int res=0;
+  // String ? op=stdin.readLineSync();
+  // switch(op)
+  // {
+  //   case'+':
+  //     res=x+y;
+  //     break;
+  //   case'-':
+  //     res=x-y;
+  //     break;
+  //   case'*':
+  //     res=x*y;
+  //     break;
+  //   case'/':
+  //     res= x~/y;
+  //     break;
+  // }
+  // print(res);
+
+  //task3
+  // String ? s = stdin.readLineSync()!;
+  // String rev = s.split('').reversed.join('');
+  // if(rev==s)
+  //   print ('Plaindrom');
+  // else
+  //   print('Not Plaindrom');
+}
